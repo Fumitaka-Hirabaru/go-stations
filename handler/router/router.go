f7a86a3a0f40cad9e9fa449247752ad44d9828ab
hirabarufumitaka <114893728+hirabarufumitaka@users.noreply.github.com>
2023-04-10 18:15:53 +0900

    fix: middlewareが複数ある時にネストが深くならないようにする

--- handler/router/router.go
+++ handler/router/router.go
@@ -9,15 +9,26 @@ import (
 	"github.com/TechBowl-japan/go-stations/service"
 )
 
+func applyMiddleware(h http.Handler, mws ...func(http.Handler) http.Handler) http.Handler {
+	for i := len(mws) - 1; i >= 0; i-- {
+		h = mws[i](h)
+	}
+	return h
+}
+
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	// register routes
 	mux := http.NewServeMux()
 	svc := service.NewTODOService(todoDB)
+	middlewares := []func(http.Handler) http.Handler{
+		middleware.UserAgentContext,
+		middleware.AccessLogMiddleware,
+	}
 	mux.HandleFunc("/todos", handler.NewTODOHandler(svc).ServeHTTP)
 	mux.HandleFunc("/healthz", handler.NewHealthzHandler().ServeHTTP)
 	mux.HandleFunc("/panic", handler.NewPanicHandler().ServeHTTP)
 	mux.HandleFunc("/do-panic", middleware.Recovery(handler.NewPanicHandler()).ServeHTTP)
 	mux.HandleFunc("/user-agent", middleware.UserAgentContext(handler.NewUserAgentHandler()).ServeHTTP)
-	mux.HandleFunc("/access-log", middleware.UserAgentContext(middleware.AccessLogMiddleware(handler.NewAccessLogHandler())).ServeHTTP)
+	mux.HandleFunc("/access-log", applyMiddleware(handler.NewAccessLogHandler(), middlewares...).ServeHTTP)
 	return mux
 }
