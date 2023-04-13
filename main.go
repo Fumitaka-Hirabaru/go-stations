c57823938804927ae2f64b001c6ec95dfa544c94
hirabarufumitaka <114893728+hirabarufumitaka@users.noreply.github.com>
2023-04-13 16:43:26 +0900

    HTTPサーバーを安全に終了させよう

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 
 	"github.com/TechBowl-japan/go-stations/db"
@@ -51,8 +55,45 @@ func realMain() error {
 	// NOTE: 新しいエンドポイントの登録はrouter.NewRouterの内部で行うようにする
 	mux := router.NewRouter(todoDB)
 
-	// * HTTPSサーバーを立ち上げる
-	http.ListenAndServe(port, mux)
+	// * HTTPサーバーを立ち上げる
+	server := &http.Server{
+		Addr:    port,
+		Handler: mux,
+	}
+
+	// シグナルを受け取るチャネルを用意する
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGTERM, os.Interrupt)
+
+	// ウェイトグループを用意する
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	// Graceful Shutdown を行うためのゴルーチン
+	go func() {
+		defer wg.Done()
+
+		// シグナルを受け取るまで待つ
+		<-sigCh
+		log.Println("Shutting down...")
+
+		// コンテキストを作成する
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		// Graceful Shutdown を実行する
+		if err := server.Shutdown(ctx); err != nil {
+			log.Println("Graceful Shutdown:", err)
+		}
+	}()
+
+	log.Println("Starting server...")
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		return err
+	}
+
+	// ウェイトグループを待つ
+	wg.Wait()
 
 	return nil
 }
